a8d010f8755352436d595bc45cd7abbfbd599762
Vaibhav Singh <singhvaibhav654@gmail.com>
2023-10-16 15:23:16 +0530

    paymentgateway add

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ const cors = require('cors');
 const app = express();
 const userRouter= require('../routes/userRoutes.js');
 const mainRouter =require('../routes/mainRouter.js')
+const paymentRoute =require('../routes/paymentRouter.js')
+require('dotenv').
+config({ path: './freelance_backend/config/config.env' });
+
+const Razorpay = require( "razorpay");
 
 app.use(express.json());
 app.use(cors());
@@ -24,8 +29,18 @@ mongoose.connection
     
 app.use('/user', userRouter);
 app.use('/main',mainRouter);
+app.use("/api", paymentRoute); 
+
+
+console.log('process.env.Razorpay_API_KEY', process.env.Razorpay_API_KEY);
+
+ const instance = new Razorpay({
+    key_id: "rzp_test_0ePEc6KzE8DkDE",
+    key_secret: process.env.RAZORPAY_APT_SECRET,
+  });
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
 
+module.exports={instance};
